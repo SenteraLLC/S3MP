d6261510dc642c68a5de5ae07af75a8bb4dbc22b
Joshua Dean <deanx252@umn.edu>
2022-09-29 19:23:00 -0500

    Sort of make TQDM work with multipart resumes.

--- S3MP/multipart_uploads.py
+++ S3MP/multipart_uploads.py
@@ -3,6 +3,7 @@
 # from multiprocessing import Process, Queue
 import concurrent.futures
 import math
+import S3MP
 from S3MP.async_utils import sync_gather_threads
 from S3MP.global_config import S3MPConfig
 from S3MP.transfer_configs import MB
@@ -29,16 +30,16 @@ def resume_multipart_upload(
     """Start or resume a multipart upload from a mirror path."""
     mpu = get_mpu(mirror_path)
     if not mpu:
-        print("Multipart upload not found, starting new one.")
+        print("\nMultipart upload not found, starting new one.")
         return mirror_path.upload_from_mirror_if_not_present()
 
     mpu_parts = list(mpu.parts.all())
     mpu_parts.sort(key=lambda part: part.part_number)
 
     total_size_bytes = mirror_path.get_size_bytes(on_s3=False)
-    part_size = mpu_parts[0].size
-    n_total_parts = math.ceil(total_size_bytes / part_size)
     n_uploaded_parts = len(mpu_parts)
+    part_size = max(part.size for part in mpu_parts)
+    n_total_parts = math.ceil(total_size_bytes / part_size)
 
     mpu_dict = {
         "Parts": [
@@ -47,15 +48,16 @@ def resume_multipart_upload(
     }
 
     with open(mirror_path.local_path, "rb") as f:
-        f.seek(part_size * n_uploaded_parts)
-        thread_futures = []
-        uploaded_parts = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+            # Verify existing parts.
+            assert(all(part.size == part_size for part in mpu_parts))
+
+            f.seek(part_size * n_uploaded_parts)
+            S3MPConfig.callback(part_size * n_uploaded_parts)
+            thread_futures = []
+            uploaded_parts = []
             for part_number in range(n_uploaded_parts + 1, n_total_parts + 1):
                 current_data = f.read(part_size)
-                # print(
-                #     f"Currrent data size: {len(current_data)}, part size: {part_size}"
-                # )
                 part = mpu.Part(part_number)
                 uploaded_parts.append(part)
                 thread_futures.append(executor.submit(part.upload, Body=current_data))
@@ -66,6 +68,7 @@ def resume_multipart_upload(
                         "PartNumber": u_part.part_number,
                     }
                 )
+                S3MPConfig.callback(part_size)
 
     mpu.complete(
         MultipartUpload={
