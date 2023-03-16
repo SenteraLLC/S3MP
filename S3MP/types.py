b2ef12e5205fcefdfd9656c5146be290f60b5f00
Joshua Dean <deanx252@umn.edu>
2023-03-16 12:30:07 -0500

    Make types compatible with pre-python3.10 versions.

--- S3MP/types.py
+++ S3MP/types.py
@@ -4,9 +4,9 @@ from mypy_boto3_s3 import S3Client, S3ServiceResource as S3Resource
 from mypy_boto3_s3.service_resource import Bucket as S3Bucket
 from mypy_boto3_s3.type_defs import ListObjectsV2OutputTypeDef as S3ListObjectV2Output
 from s3transfer.manager import TransferConfig as S3TransferConfig
-from typing import TypeVar, List
+from typing import TypeVar, List, Union
 
 T = TypeVar('T')
-SList = List[T] | T 
+SList = Union[List[T], T]
 PathSList = SList[Path]
 StrSSlist = SList[str]
