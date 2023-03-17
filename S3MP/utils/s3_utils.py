19a20c0a362499907c49decefdf0d09a5b0f9c41
Joshua Dean <deanx252@umn.edu>
2023-03-17 13:31:12 -0500

    Move Nonetype guards into functions to avoid import nightmares.

--- S3MP/utils/s3_utils.py
+++ S3MP/utils/s3_utils.py
@@ -7,20 +7,24 @@ from botocore.exceptions import ClientError
 
 def s3_list_single_key(
     key: str,
-    bucket: S3Bucket = S3MPConfig.bucket,
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> S3ListObjectV2Output:
     """List details of a single key on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     return client.list_objects_v2(
         Bucket=bucket.name, Prefix=key, Delimiter="/", MaxKeys=1
     )
 
 def s3_list_child_keys(
     key: str,
-    bucket: S3Bucket = S3MPConfig.bucket,
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> S3ListObjectV2Output:
     """List details of all child keys on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     return client.list_objects_v2(
         Bucket=bucket.name, Prefix=key, Delimiter="/"
     )
@@ -28,10 +32,12 @@ def s3_list_child_keys(
 def download_key(
     key: str,
     local_path: Path,
-    bucket: S3Bucket = S3MPConfig.bucket,
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> None:
     """Download a key from S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     if key_is_file_on_s3(key, bucket, client):
         local_path.parent.mkdir(parents=True, exist_ok=True)
         client.download_file(bucket.name, key, str(local_path), Callback=S3MPConfig.callback, Config=S3MPConfig.transfer_config)
@@ -42,10 +48,12 @@ def download_key(
 def upload_to_key(
     key: str,
     local_path: Path,
-    bucket: S3Bucket = S3MPConfig.bucket,
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> None:
     """Upload a file or folder to a key on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     if local_path.is_file():
         client.upload_file(str(local_path), bucket.name, key, Callback=S3MPConfig.callback, Config=S3MPConfig.transfer_config)
     else:
@@ -53,28 +61,39 @@ def upload_to_key(
             upload_to_key(f"{key}/{child.name}", child, bucket, client)
 
 def key_exists_on_s3(
-    key: str, bucket: S3Bucket = S3MPConfig.bucket, client: S3Client = S3MPConfig.s3_client
+    key: str,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> bool:
     """Check if a key exists on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     res = s3_list_single_key(key, bucket, client)
     return "Contents" in res or "CommonPrefixes" in res
 
 
-# @functools.cache(maxsize=1000)
 def key_is_file_on_s3(
     key: str, 
-    bucket: S3Bucket = S3MPConfig.bucket, 
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> bool:
     """Check if a key is a file on S3, returns false if it is a folder. Raises an error if the key does not exist."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     if not key_exists_on_s3(key, bucket, client):
         raise ValueError("Key does not exist on S3")
     res = s3_list_single_key(key, bucket, client)
     return "Contents" in res
 
 
-def key_size_on_s3(key: str, bucket: S3Bucket = S3MPConfig.bucket, client: S3Client = S3MPConfig.s3_client) -> int:
+def key_size_on_s3(
+    key: str,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
+    ) -> int:
     """Get the size of a key on S3. Raises an error if the key does not exist."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     if not key_exists_on_s3(key, bucket, client):
         raise ValueError("Key does not exist on S3")
     res = s3_list_single_key(key, bucket, client)
@@ -83,20 +102,24 @@ def key_size_on_s3(key: str, bucket: S3Bucket = S3MPConfig.bucket, client: S3Cli
 
 def delete_child_keys_on_s3(
     key: str,
-    bucket: S3Bucket = S3MPConfig.bucket,
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> None:
     """Delete all keys that are children of a key on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     for obj in s3_list_child_keys(key, bucket, client)["Contents"]:
         client.delete_object(Bucket=bucket.name, Key=obj["Key"])
 
 
 def delete_key_on_s3(
     key: str,
-    bucket: S3Bucket = S3MPConfig.bucket,  # TODO decide if we want to use the global config here like this
-    client: S3Client = S3MPConfig.s3_client,
+    bucket: S3Bucket = None,
+    client: S3Client = None,
 ) -> None:
     """Delete a key on S3."""
+    bucket = bucket or S3MPConfig.bucket
+    client = client or S3MPConfig.s3_client
     if not key_exists_on_s3(key, bucket, client):
         return
     if key_is_file_on_s3(key, bucket, client):
